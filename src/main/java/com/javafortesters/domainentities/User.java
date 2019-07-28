49bc0e96a9d026693ad4c1bd82c0c907de044714
AlexSmetanin <AlexSmetanin72@gmail.com>
2019-07-28 14:37:10 +0300

    Construtrors added

--- src/main/java/com/javafortesters/domainentities/User.java
+++ src/main/java/com/javafortesters/domainentities/User.java
@@ -1,4 +1,33 @@
 package com.javafortesters.domainentities;
 
 public class User {
+
+    private String username;
+    private String password;
+
+    /*public User(){                                    // конструктор по-умолчанию без агрументов
+        username = "username";
+        password = "password";
+    }*/
+
+    public User(){                                      // конструктор по-умолчанию без аргументов, который
+        this("username","password");  // вызывает коснтруктор с аргументами
+    }
+
+    public User(String username, String password){      // конструктор с агрументами
+        this.username = username;                       // this. обращается к полю текущего объекта
+        this.password = password;
+    }
+
+    public String getUsername() {                       // getter - получает значение переменной или поля
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- src/test/java/com/javafortesters/domainentities/examples/UserTest.java
+++ src/test/java/com/javafortesters/domainentities/examples/UserTest.java
@@ -1,4 +1,56 @@
 package com.javafortesters.domainentities.examples;
 
+import com.javafortesters.domainentities.User;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class UserTest {
+
+    @Test
+    public void canConstructANewUser(){
+        User user = new User();
+    }
+
+    @Test
+    public void userHasDefaultUsernameAndPassword(){
+        User user = new User();
+
+        assertEquals("default username expacted",
+                "username",
+                user.getUsername());
+
+        assertEquals("default password expacted",
+                "password",
+                user.getPassword());
+    }
+
+    /* @Test
+    public void userCanChangeDefaultValues(){ // username and password must be public
+        User auser = new User();
+        auser.username = "bob";
+        assertEquals("not default username", "bob", auser.username);
+    }*/
+
+    @Test
+    public void canCanstructorWithUsernameAndPassword(){
+        User user = new User("admin","pA55w0rD");
+
+        assertEquals("given username expected",
+                "admin",
+                user.getUsername());
+
+        assertEquals("given password expected",
+                "pA55w0rD",
+                user.getPassword());
+    }
+
+    @Test
+    public void canSetPasswordAfterConstructed(){
+        User user = new User();
+
+        user.setPassword("PaZZwor6");
+
+        assertEquals("setter password expected", "PaZZwor6", user.getPassword());
+    }
 }

fe260f7b2ff68737be1a6a9c6ea69b820cb47292
AlexSmetanin <AlexSmetanin72@gmail.com>
2019-07-29 11:25:58 +0300

    ClassExample added

---
+++ src/test/java/com/javafortesters/basicsofjavarevisited/examples/ClassExample.java
@@ -0,0 +1,23 @@
+package com.javafortesters.basicsofjavarevisited.examples;
+
+public class ClassExample {
+
+    public static final String CONSTANT = "a constant string";
+    public static String aClassField = "a class field";
+    protected static String proField = "a class field";
+    public String pubField = "a public field";
+    private String privField = "a private field";
+    private String name;
+
+    public ClassExpample(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+}
